8b2f0c00041c0158a6648740dfbbe9491073c9a6
youngchul Noh <youngchulnoh@MPC-12243989.local>
2022-12-13 15:01:18 +0900

    push

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+var swaggerJsdoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
+
 const indexRouter = require('./src/routes/index');
 const adminRouter = require('./src/routes/admin');
 const apiRouter = require('./src/api/routes');
@@ -23,6 +26,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 app.use('/api', apiRouter);
+
+//swagger
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "GA Potal Admin API",
+      version: "1.0.0",
+      description:
+        "",
+    }
+  },
+  apis: ["./src/api/routes/*.js"],
+};
+const specs = swaggerJsdoc(options);
+app.use("/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/api/routes/blogs.js
+++ src/api/routes/blogs.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const controllers = require('../controllers');
 
+/**
+* @swagger
+* paths:
+*  /api/blogs/categories:
+*   get:
+*     tags: [Blogs]
+*     summary: 카테고리 목록
+*     responses:
+*       "200":
+*         description:
+*/
 router.get('/categories', async (req, res, next) => {
     await controllers.blogs.getCategories(req, res);
 });
