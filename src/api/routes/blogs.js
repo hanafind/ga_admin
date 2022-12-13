6a2f3b53b99b7a25ef20bb106ba824cd5fe0e92a
youngchul Noh <youngchulnoh@MPC-12243989.local>
2022-12-13 12:46:22 +0900

    push

--- src/api/routes/blogs.js
+++ src/api/routes/blogs.js
@@ -4,20 +4,8 @@ const router = express.Router();
 const controllers = require('../controllers');
 
 router.get('/', async (req, res, next) => {
-    const { Client } = require('pg')
-    const client = new Client({
-        host: '192.168.10.33',
-        port: 5432,
-        user: 'local',
-        password: '12341234',
-        database: 'dblgap'
-      })
-    await client.connect()
     
-    const res1 = await client.query('select * from public.post_categories', []);
-    console.log(res1.rows[0]) // Hello world!
-    await client.end()
-    res.send(res1.rows[0])
+    res.send('ㅎㅎㅎㅎㅎ')
 });
 
 module.exports = router;
---
+++ src/modules/pg.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+const config = require(`../../config/${process.env.NODE_ENV}.json`);
+
+const client = new Client(config.postgesql);
+
+const query = async(sql, values)=>{
+    await client.connect();
+    const res = await client.query(sql, values);
+    await client.end();
+    return res.rows;
+
+};
+
+module.exports = {
+    query: query
+}
