ffaf36c5cc2e96671463e9dacc12d9f976a39efa
hanafind <hanadevhana@gmail.com>
2023-01-10 14:24:18 +0900

    run

--- run.sh
+++ run.sh
@@ -2,12 +2,15 @@
 if [ -z $1 ]
 then
     echo "No Parameter Passed..."
+    echo "'local','development', 'production'"
 elif [ $1 = "local" ] || [ $1 = "development" ] || [ $1 = "production" ]
 then
-    echo "Start $1"
+    echo "yarn cache clean"
+    yarn cache clean
+    echo "yarn install --offline"
+    yarn install --offline
     echo "Shut Down pm2"
-    pm2 delete all
-    echo "Start pm2"
+    pm2 delete "GA_PORTAL_ADMIN"
     npm run $1
     echo "End"
 else
